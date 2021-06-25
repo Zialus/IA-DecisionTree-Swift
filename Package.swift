dd5a00932951d1d2aa187b59186c188852730c8d
Raul Ferreira <raulmendesferreira@gmail.com>
2021-06-25 22:46:26 +0100

    Update swift version

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
     name: "DecisionTree",
     dependencies: [
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0")
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.0")
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "DecisionTree",
             dependencies: ["Rainbow"]
         )
